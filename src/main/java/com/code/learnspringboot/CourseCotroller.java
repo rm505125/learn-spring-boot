2b82d8c434f57521015aaf25ae6183d4a69659a7
Rahul Maurya <rm505125@gmail.com>
2022-12-29 16:22:47 +0530

    first commit

---
+++ src/main/java/com/code/learnspringboot/CourseCotroller.java
@@ -0,0 +1,18 @@
+package com.code.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseCotroller {
+
+	@GetMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(new Course(1, "Learn AWS", "rahul"),
+				new Course(2, "Learn sping boot", "rahul"));
+	}
+
+}
